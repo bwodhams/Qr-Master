2bb84a6121ad21f1c0b47d6eb0c3993383b3b745
bwodhams <bwodhams@email.arizona.edu>
2019-04-17 11:44:07 -0700

    Added client side testing to lower num requests sent to server.

--- backend/server/public/javascripts/user/register.js
+++ backend/server/public/javascripts/user/register.js
@@ -1,15 +1,23 @@
 function register() {
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var password = document.getElementById('password').value;
-    var confirmPassword = document.getElementById('confirmPassword').value;
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var password = document.getElementById('password');
+    var confirmPassword = document.getElementById('confirmPassword');
+    var submitBtn = document.getElementById('registerBtn');
+    name.readOnly = true;
+    email.readOnly = true;
+    password.readOnly = true;
+    confirmPassword.readOnly = true;
+    submitBtn.disabled = true;
     var serverResponse = document.getElementById('serverResponse');
 
-    if (name == "" || email == "" || password == "" || confirmPassword == "") {
+    if (name.value == "" || email.value == "" || password.value == "" || confirmPassword.value == "") {
         serverResponse.innerHTML = "<span class='red-response'>All fields required.</span>"
     } else {
-        if (password != confirmPassword) {
-            serverResponse.innerHTML = "<span class='red-response'>Passwords don't match.</span>"
+        var errorCheck = registrationErrorCheck(name.value, email.value, password.value, confirmPassword.value);
+        if (errorCheck.length > 0) {
+            serverResponse.innerHTML = "<span class='red-response'>" + errorCheck + "</span>";
+            resetFields();
         } else {
             serverResponse.innerHTML = "";
             var form = document.getElementById('inputForm');
@@ -24,10 +32,10 @@ function register() {
             xhr.setRequestHeader('Content-type', 'application/json');
             xhr.send(
                 JSON.stringify({
-                    name: name,
-                    email: email,
-                    password: password,
-                    confirmPassword: confirmPassword
+                    name: name.value,
+                    email: email.value,
+                    password: password.value,
+                    confirmPassword: confirmPassword.value
                 })
             );
         }
@@ -42,7 +50,8 @@ function registerResponse() {
         document.getElementById('serverResponse').innerHTML = "<span class='red-response'>" + "Account created successfully" + "</span>";
         //window.location.href = "/";
     } else {
-        document.getElementById('inputForm').form.style.filter = "";
+        resetFields();
+        document.getElementById('inputForm').style.filter = "";
         document.getElementById('loadingImg').innerHTML = "";
         document.getElementById('serverResponse').innerHTML = "<span class='red-response'>" + this.response.message + "</span>";
     }
@@ -50,3 +59,50 @@ function registerResponse() {
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('registerBtn').addEventListener('click', register);
 });
+
+function resetFields() {
+    document.getElementById('name').readOnly = false;
+    document.getElementById('email').readOnly = false;;
+    document.getElementById('password').readOnly = false;;
+    document.getElementById('confirmPassword').readOnly = false;;
+    document.getElementById('registerBtn').disabled = false;
+}
+
+function registrationErrorCheck(fullName, email, password, confirmPassword) {
+    let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    let pwdLowerReg = /[a-z]+/;
+    let pwdUpperReg = /[A-Z]+/;
+    let pwdNumReg = /.*\d.*/;
+
+    let outputString = '';
+
+    if (fullName.length < 1) {
+        outputString += ' Invalid name. ';
+    }
+
+    if (!emailReg.test(email)) {
+        outputString += ' Invalid email. ';
+    }
+
+    if (password.length < 6 || password.length > 20) {
+        outputString += ' Password must be longer than 6 characters and less than 20 characters. ';
+    }
+
+    if (!pwdLowerReg.test(password)) {
+        outputString += ' Password must contain at least one lowercase character. ';
+    }
+
+    if (!pwdUpperReg.test(password)) {
+        outputString += ' Password must contain at least one uppercase character. ';
+    }
+
+    if (!pwdNumReg.test(password)) {
+        outputString += ' Password must contain at least one digit. ';
+    }
+
+    if (password != confirmPassword) {
+        outputString += " Password and confirmation password don't match. ";
+    }
+
+    return outputString;
+}
