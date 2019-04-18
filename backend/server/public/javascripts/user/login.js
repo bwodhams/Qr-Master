dcf2a7701243921bc447330b244459841e67f310
bwodhams <bwodhams@email.arizona.edu>
2019-04-18 16:20:43 -0700

    Added client side testing of input on login to lower num requests to server.

--- backend/server/public/javascripts/user/login.js
+++ backend/server/public/javascripts/user/login.js
@@ -6,17 +6,23 @@ function login() {
     if (email == "" || password == "") {
         serverResponse.innerHTML = "<span class='red-response'>Email and password fields required.</span>"
     } else {
-        var xhr = new XMLHttpRequest();
-        xhr.addEventListener('load', loginResponse);
-        xhr.responseType = 'json';
-        xhr.open('POST', '/api/user/login');
-        xhr.setRequestHeader('Content-type', 'application/json');
-        xhr.send(
-            JSON.stringify({
-                email: email,
-                inputPassword: password
-            })
-        );
+        var loginErrorCheckResponse = loginErrorCheck(email, password);
+        if (loginErrorCheckResponse.length > 0) {
+            serverResponse.innerHTML = "<span class='red-response'>" + loginErrorCheckResponse + "</span>";
+        } else {
+            var xhr = new XMLHttpRequest();
+            xhr.addEventListener('load', loginResponse);
+            xhr.responseType = 'json';
+            xhr.open('POST', '/api/user/login');
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.send(
+                JSON.stringify({
+                    email: email,
+                    inputPassword: password
+                })
+            );
+        }
+
     }
 }
 
@@ -31,3 +37,40 @@ function loginResponse() {
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('signin').addEventListener('click', login);
 });
+
+function loginErrorCheck(email, password) {
+    let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    let pwdLowerReg = /[a-z]+/;
+    let pwdUpperReg = /[A-Z]+/;
+    let pwdNumReg = /.*\d.*/;
+
+    let outputString = '';
+
+
+    if (!emailReg.test(email)) {
+        outputString += ' Invalid email. ';
+    }
+
+    if (password.length < 6 || password.length > 20) {
+        outputString += ' Incorrect password. ';
+    }
+
+    if (!pwdLowerReg.test(password)) {
+        if (!(outputString.indexOf('Incorrect password') > 0)) {
+            outputString += ' Incorrect password. ';
+        }
+    }
+
+    if (!pwdUpperReg.test(password)) {
+        if (!(outputString.indexOf('Incorrect password') > 0)) {
+            outputString += ' Incorrect password. ';
+        }
+    }
+
+    if (!pwdNumReg.test(password)) {
+        if (!(outputString.indexOf('Incorrect password') > 0)) {
+            outputString += ' Incorrect password. ';
+        }
+    }
+    return outputString;
+}
