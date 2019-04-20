9cdf91af001124513eba5ebbf8bc2ce63a45260d
bwodhams <bpwodhams@gmail.com>
2019-04-20 01:24:23 -0700

    Login page now has "forgot password" functionality. :)

--- backend/server/frontend/public/javascripts/user/login.js
+++ backend/server/frontend/public/javascripts/user/login.js
@@ -1,3 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('signin').addEventListener('click', login);
+    document.getElementById('forgotPassword').addEventListener('click', forgotPassword);
+});
+
 function login() {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
@@ -35,9 +40,55 @@ function loginResponse() {
         document.getElementById('serverResponse').innerHTML = "<span class='red-response'>" + this.response.message + "</span>";
     }
 }
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('signin').addEventListener('click', login);
-});
+
+function forgotPassword() {
+    var passwordField = document.getElementById('password');
+    var signinBtn = document.getElementById('signin');
+    var forgotPasswordText = document.getElementById('forgotPassword');
+    var formDiv = document.getElementById('form');
+    var submitBtn = document.createElement('button');
+    passwordField.parentNode.removeChild(passwordField);
+    signinBtn.parentNode.removeChild(signinBtn);
+    forgotPasswordText.parentNode.removeChild(forgotPasswordText);
+    for (var i = 0; i < document.getElementsByClassName('breaks').length - 2; i++) {
+        document.getElementsByClassName('breaks')[i].style.display = "none";
+    }
+    submitBtn.id = "submitBtn";
+    submitBtn.innerHTML = "submit";
+    formDiv.appendChild(submitBtn);
+    document.getElementById('submitBtn').addEventListener('click', resetPassword);
+}
+
+function resetPassword() {
+    var email = document.getElementById('email').value;
+    var serverResponse = document.getElementById('serverResponse');
+    var emailCheck = emailErrorCheck(email);
+
+    if (emailCheck.length > 0) {
+        serverResponse.innerHTML = "<span class='red-response'>" + emailCheck + "</span>";
+    } else {
+        serverResponse.innerHTML = "";
+        var xhr = new XMLHttpRequest();
+        xhr.addEventListener('load', resetPasswordResponse);
+        xhr.responseType = 'json';
+        xhr.open('POST', '/api/user/forgotPassword');
+        xhr.setRequestHeader('Content-type', 'application/json');
+        xhr.send(
+            JSON.stringify({
+                email: email
+            })
+        );
+    }
+
+}
+
+function resetPasswordResponse() {
+    if (this.status === 201) {
+        console.log("successful forgot password request");
+    } else {
+        document.getElementById('serverResponse').innerHTML = "<span class='red-response'>" + this.response.message + "</span>";
+    }
+}
 
 function loginErrorCheck(email, password) {
     let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
@@ -72,6 +123,15 @@ function loginErrorCheck(email, password) {
         if (!(outputString.indexOf('Incorrect password') > 0)) {
             outputString += ' Incorrect password. ';
         }
+    }
+    return outputString;
+}
+
+function emailErrorCheck(email) {
+    let emailReg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
+    let outputString = '';
+    if (!emailReg.test(email)) {
+        outputString += ' Invalid email. ';
     }
     return outputString;
 }
