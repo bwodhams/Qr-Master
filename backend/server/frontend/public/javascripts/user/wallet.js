46d440e5297da7cb6a858a078350396fbbae09e1
bwodhams <bwodhams@email.arizona.edu>
2019-04-24 13:53:56 -0700

    Wallet page gets your cards. Doesn't display properly though. WIP!!

--- backend/server/frontend/public/javascripts/user/wallet.js
+++ backend/server/frontend/public/javascripts/user/wallet.js
@@ -7,12 +7,43 @@
  */
 
 document.addEventListener("DOMContentLoaded", function () {
+    var loadingCircle = document.getElementById('loadingImg');
+    loadingCircle.innerHTML = '<img src="../images/loading_screen.svg" alt="loading" height="125px" width="125px">';
+    getCards();
     document.getElementById('addNewCardBtn').addEventListener('click', prepareAddNewCard);
 });
 
+function getCards() {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', getCardsResponse);
+    xhr.responseType = 'json';
+    xhr.open('GET', '/api/user/getCards/' + getCookie("accEmail"));
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('authorization', localStorage.getItem('qr4gloginAuthTokenDesktop'));
+    xhr.send();
+}
+
+function getCardsResponse() {
+    console.log(this.response);
+    if (this.status === 200) {
+        var loadingCircle = document.getElementById('loadingImg');
+        var allCards = document.getElementById('myCards');
+        var addNewCardBtn = document.getElementById('addNewCardBtn');
+        var outputHTML = "";
+        for (var i = 0; i < this.response.name.length; i++) {
+            outputHTML += "name = " + this.response.name[i] + " CCLastDigits = " + this.response.creditCardLastDigits[i] + "<br>";
+        }
+        allCards.innerHTML = outputHTML;
+        loadingCircle.style.display = "none";
+        addNewCardBtn.style.display = "";
+    }
+}
+
 function prepareAddNewCard() {
+    var myCardsContainer = document.getElementById('myCardsContainer');
     var addNewCardDiv = document.getElementById('addNewCard');
     addNewCardDiv.style.display = "";
+    myCardsContainer.style.display = "none";
     var card = new Card({
         form: 'form',
         container: '.card',
@@ -25,4 +56,20 @@ function prepareAddNewCard() {
         width: 350, // optional — default 350px
         formatting: true
     });
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
